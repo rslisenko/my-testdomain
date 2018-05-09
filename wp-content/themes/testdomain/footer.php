ade51ac9782e2bf4ce6652cbe982b1bc66640de8
Roman <rvslisenko@gmail.com>
2018-05-09 13:40:19 +0300

    add popup window on Contact btn click

--- wp-content/themes/testdomain/footer.php
+++ wp-content/themes/testdomain/footer.php
@@ -16,6 +16,22 @@
 
 		</div><!-- #content -->
 
+		<!-- begin popup window -->
+		<div class="popup-window">
+			<span class="close-popup">X</span>
+			<div class="popup-content">
+				<h2 class="popup-title text-center">Popup title</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</div>
+		<!-- end popup window -->
+
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="wrap">
 				<?php
--- wp-content/themes/testdomain/js/global.js
+++ wp-content/themes/testdomain/js/global.js
@@ -3,6 +3,18 @@
 
 	$(document).ready(function () {
 
+		$('.contact-us-btn').on('click', function(event) {
+			$('body').addClass('overlay');
+			$('.popup-window').addClass('show-popup');
+		});
+
+		$(document).on('click', function(event) {
+			var target = event.target;
+			if(!$(target).closest('.popup-window').length && !$(target).closest('.contact-us-btn').length || $(target).is('.close-popup')){
+				$('.popup-window').removeClass('show-popup');
+				$('body').removeClass('overlay');
+			}
+		});
 	});
 
 	$(window).on( 'load', function () {
